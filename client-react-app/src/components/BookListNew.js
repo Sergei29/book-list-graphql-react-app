e82790066079a8cf67a4d5635a18b3e0480b9a42
Sergetestcargo <protopulos.pulatikos@yandex.ru>
2020-10-18 18:39:34 +0100

    reainstalled packages

--- client-react-app/src/components/BookListNew.js
+++ client-react-app/src/components/BookListNew.js
@@ -1,24 +1,23 @@
 import React from "react";
-import { gql } from "apollo-boost";
 import { Query } from "react-apollo";
 import { getBooksQuery } from "../queries/queries";
 
 const BookListNew = () => (
-	<Query query={getBooksQuery}>
-		{({ loading, error, data }) => {
-			if (loading) return <p>Loading...</p>;
-			if (error) return <p>Error: {error.message}</p>;
-			return (
-				<div>
-					<ul id="book-list-new">
-						{data.books.map((book) => (
-							<li key={book.id}>{book.name}</li>
-						))}
-					</ul>
-				</div>
-			);
-		}}
-	</Query>
+  <Query query={getBooksQuery}>
+    {({ loading, error, data }) => {
+      if (loading) return <p>Loading...</p>;
+      if (error) return <p>Error: {error.message}</p>;
+      return (
+        <div>
+          <ul id="book-list-new">
+            {data.books.map((book) => (
+              <li key={book.id}>{book.name}</li>
+            ))}
+          </ul>
+        </div>
+      );
+    }}
+  </Query>
 );
 
 export default BookListNew;
