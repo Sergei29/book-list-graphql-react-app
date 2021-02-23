a43ec0ffeb27dbc8464ec4f8ce6736ca83e56429
Sergetestcargo <protopulos.pulatikos@yandex.ru>
2021-02-23 20:51:37 +0000

    test path server

--- server/server.ts
+++ server/server.ts
@@ -46,9 +46,9 @@ apolloServer.applyMiddleware({ app, path: "/graphql" });
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "client/build")));
   //for every/all incoming get requests: serve react app index.html
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client/build", "index.html"));
-  });
+  // app.get("*", (req, res) => {
+  //   res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  // });
 
   app.get("/test", (req, res) => {
     res.send("test response from server");
