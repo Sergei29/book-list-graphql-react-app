c4b9ed247d391c737b99134689f7f713d8a0db0f
Sergei29 <34629784+Sergei29@users.noreply.github.com>
2021-02-28 18:24:47 +0000

    Back end authentication (#26)
    * removed the REST-type routing and authentication
    
    * backend: added authentication: schemas, resolvers, jwt
    
    * added entity types and server side check if item exists in DB before proceed
    
    * removed comment line
    
    Co-authored-by: Sergetestcargo <protopulos.pulatikos@yandex.ru>

--- server/types/types.ts
+++ server/types/types.ts
@@ -1,10 +1,30 @@
 import { Document } from "mongoose";
 
-export type User = {
-  id: string;
-  email: string;
+export type UserType = {
+  username: string;
   password: string;
-  companyId: string;
-};
+} & Document;
+
+export type BookType = {
+  name: string;
+  genre: string;
+  authorId: string;
+} & Document;
+
+export type AuthorType = {
+  name: string;
+  age: number;
+} & Document;
 
 export type DatabaseDocType = Document;
+
+export enum ErrorMessage {
+  BOOK_EXISTS = "Book already exists.",
+  BOOK_NOT_FOUND = "Book does not exist.",
+  AUTHOR_EXISTS = "Author already exists.",
+  AUTHOR_NOT_FOUND = "Author does not exist.",
+  USER_EXISTS = "User already exists.",
+  USER_NOT_FOUND = "User does not exist.",
+  LOGIN_REQUIRED = "Please login again.",
+  WRONG_PASSWORD = "Wrong password.",
+}
