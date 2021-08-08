bde2fcc6928bffbe6dd065c19f91baaf545e167d
Serge Basangovs <sergejs.basangovs@gmail.com>
2021-08-08 16:32:56 +0300

    removed redundant code

--- client/src/graphql/queries.ts
+++ client/src/graphql/queries.ts
@@ -53,16 +53,6 @@ export const GET_BOOK_DETAILS = gql`
   }
 `;
 
-export const GET_CURRENT_USER = gql`
-  query GetCurrentUser {
-    me {
-      id
-      email
-      role
-    }
-  }
-`;
-
 export const GET_FAVORITES = gql`
   query GetFavorites {
     arrFavoriteBookIds @client
