37b4be4837c13807873388e831c7c2384a1eb511
Sergejs Basangovs <sergejs.basangovs@gmail.com>
2022-03-27 17:58:42 +0300

    feat: add auth provider and hook

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,16 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import ApolloProvider from "./graphql/ApolloProvider/ApolloProvider";
+import { AuthProvider } from "./containers/AuthProvider";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <ApolloProvider>
-        <App />
-      </ApolloProvider>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <ApolloProvider>
+          <App />
+        </ApolloProvider>
+      </BrowserRouter>
+    </AuthProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
